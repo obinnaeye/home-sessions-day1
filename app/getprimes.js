5c2bd1cf1ae4debdff99aa359b5e804f9f85b86a
Nnenanya Obinna <obinnaeye@yahoo.com>
2017-03-13 14:36:01 +0100

    add primeCheck function

--- app/getprimes.js
+++ app/getprimes.js
@@ -17,6 +17,20 @@ module.exports = {
            
         }
 
-        return primes;         
+        return primes;      
+
+        function primeCheck(item){
+            // logically, any number greater than half of n can not modular divide n to be 0
+            // to reduce itirations, use range;
+            var rang = item < 2 ? item : Math.round(item/2);            
+            var count = 2;
+            while (count <= rang){
+                if(item % count === 0){
+                    return false;
+                }
+                count++;
+            }
+            return true;
+        }
     }
 }
